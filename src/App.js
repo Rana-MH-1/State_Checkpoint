145794667c6722674ef9c8038c0124d13da941c7
Rana-MH-1 <rana.meftah@enis.tn>
2021-05-17 02:01:40 +0100

    adding files - using state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import './style.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export class App extends Component {
+  constructor(props) {
+    super(props)
+  
+    this.state = {
+        FullName : 'Rana Meftah',
+        Profession: 'Biological engineer & full stack student',
+        Bio: 'I am a biological enginner and a full stack student.I have worked as a Quality Manager for 11 months and I am studying now web development because I am passionate about it.' ,
+        SrcImg : 'https://i.pinimg.com/originals/0c/3b/3a/0c3b3adb1a7530892e55ef36d3be6cb8.png' ,
+        Show: false,
+        date: 0
+    }
+  }
+  componentDidMount() {
+    this.timerID = setInterval(
+      () => this.tick(),
+      1000
+    );
+  }
+  
+  tick(prevState) {
+    this.setState({
+      date: this.state.date + 1
+    });
+  }
+
+
+  isToggle = () => {
+    this.setState(state => ({
+      Show : !this.state.Show
+    }))
+  }
+
+  render() {
+    const Show = this.state.Show;
+    return (
+      <div className='Profile'>
+        <h1> {this.state.FullName} </h1>
+        <h3> {this.state.Profession} </h3>
+        <p className='bio'> {this.state.Bio} </p>
+        <button onClick={this.isToggle}>Show profile image</button>
+        <br/><br/>
+        <img src={Show ? './profileimage.jpg' : this.state.SrcImg} alt='profileimge'></img>
+        <p className='Time'> The last component was mounted since {this.state.date} seconds</p>
+        
+        
+        
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
